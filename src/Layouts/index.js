46b198a48b237fdfc134178f6826b4758dc1173a
Alabs02 <alabson.inc@gmail.com>
2021-10-23 00:40:39 +0100

    configuring app

--- src/Layouts/index.js
+++
@@ -1,7 +0,0 @@
-import AdminLayout from './AdminLayout/AdminLayout';
-import StaffLayout from './StaffLayout/StaffLayout';
-
-export {
-  AdminLayout,
-  StaffLayout,
-};
--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import './App.css';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { GuardProvider, GuardedRoute } from 'react-router-guards';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import 'animate.css';
+
+// Components
+
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Alabson.inc</h1>
-      </header>
-    </div>
+    <Router>
+      <GuardProvider guards={[]} error={<div>ErrorPage</div>}>
+        <TransitionGroup>
+          <CSSTransition classNames="animate__animated animate__fadeInLeft" timeout={100}>
+            <Suspense fallback={<div className="grid place-items-center">Loading...</div>}>
+              <Switch>
+                <GuardedRoute path="/signin" exact></GuardedRoute>
+              </Switch>
+            </Suspense>
+          </CSSTransition>
+        </TransitionGroup>
+      </GuardProvider>
+    </Router>
   );
 }
 
